ed56b491335516a232ab6fb6a251e5ed68ba6e38
Katya <katya.p.1129@gmail.com>
2022-06-14 12:20:27 -0400

    add KeplerGl component

--- dash/src/App.js
+++ dash/src/App.js
@@ -1,12 +1,39 @@
 
 import './App.css';
 
+import keplerGlReducer from 'kepler.gl/reducers';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { taskMiddleware } from 'react-palm/tasks';
+import { Provider } from 'react-redux';
+import { KeplerGl } from 'kepler.gl/dist/components';
+// import { addDataToMap } from 'kepler.gl/actions';
+// import useSWR from 'swr';
+
+
+const reducers =  combineReducers({
+  keplerGl: keplerGlReducer
+});
+
+const store = createStore(reducers, {}, applyMiddleware(taskMiddleware));
+
 function App() {
   return (
-    <div className="app">
-      <h1>Geo Spacial Data Visualization Dashboard</h1>
-    </div>
-  );
+    <Provider store={store}>
+      <Map />
+    </Provider>
+  )
 }
 
 export default App;
+
+function Map() {
+  return (
+    <KeplerGl 
+      id="covid" 
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API} 
+      width={window.innerWidth}
+      height={window.innerHeight}
+      store={store}
+    />
+  )
+}
